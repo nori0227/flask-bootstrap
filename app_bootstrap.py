f626f411fb36cf64e99fd4ee4b764e3718302cd0
Jannatul Ferdous <110571832+nori0227@users.noreply.github.com>
2022-09-18 23:20:32 -0400

    Update app_bootstrap.py

--- app_bootstrap.py
+++ app_bootstrap.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('p1_homepage_bootstrap.html')
+
+@app.route('/bootstrap')
+def bootstrap():
+    return render_template('p2_homepage_bootstrap.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
 
